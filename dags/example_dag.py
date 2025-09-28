d6c811717931f8dc70e224fb071ded26c6066449
kishor lohare <loharekishor@gmail.com>
2025-09-28 16:06:07 +0530

    Initial commit

---
+++ dags/example_dag.py
@@ -0,0 +1,12 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(schedule_interval="@daily", start_date=datetime(2025, 9, 27), catchup=False, tags=["example", "airflow-vscode-docker"])
+def hello_airflow_vscode_docker():
+    @task
+    def say_hello():
+        print("Hello from airflow-vscode-docker!")
+
+    say_hello()
+
+dag = hello_airflow_vscode_docker()
---
+++ getversions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get current timestamp
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Append versions info to the file with timestamp
+(
+  echo "===== $timestamp ====="
+  echo "=== Python Version ==="
+  python --version
+  echo ""
+  echo "=== Airflow Version ==="
+  airflow version
+  echo ""
+  echo "=== Airflow Info ==="
+  airflow info
+  echo ""
+) > current_versions.txt
+
+echo "Saved current versions with timestamp to current_versions.txt"
